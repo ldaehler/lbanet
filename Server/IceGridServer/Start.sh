46687f3c01725952c81945e0f58dd7e16410b82b
vdelage <vdelage@3806491c-8dad-11de-9a8c-6d5b7d1e4d13>
2009-09-12 13:51:09 +0000

    added strating script

---
+++ Server/IceGridServer/Start.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+rm -rf db/node/*
+rm -rf db/registry/*
+icegridnode --Ice.Config=config.grid &
+sleep 5
+icegridadmin --Ice.Config=config.grid -e "application add 'application.xml'"
+icegridadmin --Ice.Config=config.grid -e "server start LBANetGlacier2"
