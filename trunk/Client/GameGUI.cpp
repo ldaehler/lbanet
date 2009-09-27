e7f48a2cef840259414b65f0f756321e7fa2610d
vdelage <vdelage@3806491c-8dad-11de-9a8c-6d5b7d1e4d13>
2009-09-27 18:07:47 +0000

    release 0.55

--- trunk/Client/GameGUI.cpp
+++ trunk/Client/GameGUI.cpp
@@ -30,9 +30,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "GameEvents.h"
 #include "DataLoader.h"
 
-#ifndef _LBANET_SET_EDITOR_
-#define _LBANET_SET_EDITOR_
-#endif
+//#ifndef _LBANET_SET_EDITOR_
+//#define _LBANET_SET_EDITOR_
+//#endif
 
 /***********************************************************
 constructor
