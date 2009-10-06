3a32fba81abc8297a4bf25b933c3190d3cab41f3
vdelage <vdelage@3806491c-8dad-11de-9a8c-6d5b7d1e4d13>
2009-10-06 22:24:03 +0000

    added website

---
+++ trunk/www/register.php
@@ -0,0 +1,133 @@
+<?php session_start();
+
+require_once('class.phpmailer.php');
+
+mysql_connect("localhost", "root", "") or die(mysql_error());
+mysql_select_db("Lbanet") or die(mysql_error());
+
+
+	if(isset($_POST['submit']))
+	{
+		$username = trim(mysql_real_escape_string($_POST['username']));
+		$password = trim(mysql_real_escape_string($_POST['password']));
+		$password2 = trim(mysql_real_escape_string($_POST['password2']));
+		$email = trim(mysql_real_escape_string($_POST['email']));
+
+
+		if($username && strlen($username) >= 4 && strlen($username) <= 15)
+		{
+			$query = mysql_query("SELECT `id` FROM `users` WHERE `username`='".$username."' LIMIT 1");
+			if(mysql_num_rows($query))
+			{
+				$error['userexists'] = ' Username already exists';
+			}
+		}
+		else
+		{
+			$error['usernameinput'] = ' Username should have between 4 and 15 characters';
+		}
+
+		if($email)
+		{
+			$query = mysql_query("SELECT `id` FROM `users` WHERE `email`='".$email."' LIMIT 1");
+			if(mysql_num_rows($query))
+			{
+				$error['emailexists'] = ' Email already in use';
+			}
+		}
+		else
+		{
+			$error['emailinput'] = ' Please supply an email address';
+		}
+
+		if($password && $password2)
+		{
+			if($password != $password2)
+			{
+				$error['passmismatch'] = ' Passwords do not match';
+			}
+
+			if(strlen($username) >= 20)
+			{
+				$error['passwordinput'] = ' Password length should be less than 20 characters';
+			}
+		}
+		else
+		{
+			$error['passwordinput'] = ' Please enter your password in both fields';
+		}
+
+		include("securimage.php");
+		$img = new Securimage();
+		$valid = $img->check($_POST['code']);
+ 		if($valid != true)
+ 		{
+			$error['codeinput'] = ' The code entered is incorrect';
+ 		}
+
+
+		if(!isset($error))
+		{
+			$activationKey =  mt_rand() . mt_rand() . mt_rand() . mt_rand() . mt_rand();
+			$sql="INSERT INTO users (username, password, email, activationkey, status, creationdate) VALUES ('$username', '$password', '$email', '$activationKey', '0', NOW())";
+
+			if (!mysql_query($sql))
+			{
+				die('Error: ' . mysql_error());
+			}
+
+
+
+			##Send activation Email
+			$mail = new PHPMailer ();
+			$mail->IsSMTP ();
+			$mail->SMTPDebug  = 1;
+			$mail->SMTPAuth   = true;
+			$mail->SMTPSecure = "tls";
+			$mail->Host = "smtp.gmail.com";
+			$mail->Port  = 587;
+			$mail->Username   = "lbanet.game@googlemail.com";
+			$mail->Password   = "lapichonmelba";
+			$mail->AddReplyTo ("noreply@lbanet.com", "Lbanet");
+			$mail->SetFrom("noreply@lbanet.com", "Lbanet");
+			$mail->Subject = "Lbanet Registration";
+			$mail->Body = "Welcome to Lbanet!<br /><br />You, or someone using your email address, has registered to Lbanet. You can complete registration by clicking the following link:<br /> http://127.0.0.1/verifyemail.php?$activationKey <br /><br />If this is an error, ignore this email and you will be removed from our database after 24h.<br /><br />Regards,<br /> Lbanet Team";
+			$mail->AddAddress ("$email", "$username");
+			$mail->IsHTML (true);
+			if(!$mail->Send())
+			{
+				echo "Mailer Error: " . $mail->ErrorInfo;
+			}
+			else
+			{
+				die("An email has been sent to $email with an activation key. Please check your mail to complete registration.");
+			}
+		}
+	}
+
+
+
+?>
+
+<form name="reg" method="post" enctype="application/x-www-form-urlencoded">
+	<div style="width: 120px; float: left; height: 25px">Username:</div><input type="text" name="username" /><?php if(isset($error['userexists'])){ echo $error['userexists'];} if(isset($error['usernameinput'])){ echo $error['usernameinput'];} ?><br /><br />
+	<div style="width: 120px; float: left; height: 25px">Email: </div><input type="text" name="email" /><?php if(isset($error['emailexists'])){ echo $error['emailexists'];} if(isset($error['emailinput'])){ echo $error['emailinput'];} ?><br /><br />
+	<div style="width: 120px; float: left; height: 25px">Password: </div><input type="password" name="password" /><?php if(isset($error['passmismatch'])){ echo $error['passmismatch'];} if(isset($error['passwordinput'])){ echo $error['passwordinput'];} ?><br/><br />
+	<div style="width: 120px; float: left; height: 25px">Confirm password: </div><input type="password" name="password2" /><br/><br />
+
+
+	<div style="width: 120px; float: left; height: 55px">Security Code:</div>
+
+	<div style="width: 430px; float: left; height: 55px">
+		<img src="securimage_show.php?sid=<?php echo md5(uniqid(time())); ?>" id="siimage" align="left" style="padding-right: 5px; border: 0" />
+		<a tabindex="-1" style="border-style: none" href="#" title="Refresh Image" onclick="document.getElementById('siimage').src = 'securimage_show.php?sid=' + Math.random(); return false"><img src="/images/refresh.gif" alt="Reload Image" border="0" onclick="this.blur()" align="bottom" /></a>
+	</div>
+	<br/><br/><br/>
+
+	<div style="width: 120px; float: left; height: 25px">Verify Code:</div>
+	<input type="text" name="code" /><?php if(isset($error['codeinput'])){ echo $error['codeinput'];}?><br/>
+	<br/>
+	<input type="submit" name="submit" value="Register" />
+</form>
+
+
---
+++ trunk/www/verifyemail.php
@@ -0,0 +1,60 @@
+<?php
+require_once('class.phpmailer.php');
+
+mysql_connect("localhost", "root", "") or die(mysql_error());
+mysql_select_db("Lbanet") or die(mysql_error());
+
+
+##User isn't registering, check verify code and change activation code to null, status to activated on success
+
+$queryString = $_SERVER['QUERY_STRING'];
+
+$query = "SELECT * FROM users";
+
+$result = mysql_query($query) or die(mysql_error());
+
+while($row = mysql_fetch_array($result))
+{
+
+	if ($queryString == $row["activationkey"])
+	{
+		$login =  $row["username"];
+		$pass =  $row["password"];
+		$to =  $row["email"];
+
+		echo "Congratulations! $login is now the proud new owner of an Lbanet account. <br />";
+		echo "You will receive shortly a confirmation email with a summary of your account information.";
+
+		$sql="UPDATE users SET activationkey = '$row[id]', status='1' WHERE (id = $row[id])";
+
+		if (!mysql_query($sql))
+		{
+			die('Error: ' . mysql_error());
+		}
+
+
+		##Send confirmation Email
+		$mail = new PHPMailer ();
+		$mail->IsSMTP ();
+		$mail->SMTPDebug  = 1;
+		$mail->SMTPAuth   = true;
+		$mail->SMTPSecure = "tls";
+		$mail->Host = "smtp.gmail.com";
+		$mail->Port  = 587;
+		$mail->Username   = "lbanet.game@googlemail.com";
+		$mail->Password   = "lapichonmelba";
+		$mail->AddReplyTo ("noreply@lbanet.com", "Lbanet");
+		$mail->SetFrom("noreply@lbanet.com", "Lbanet");
+		$mail->Subject = "Lbanet account confirmation";
+		$mail->Body = "Welcome to Lbanet!<br /><br />You have successfully registered to Lbanet. Your account information:<br />Login: \"$login\"<br /> Password: \"$pass\"<br /><br />Regards,<br /> Lbanet Team";
+		$mail->AddAddress ("$to", "$login");
+		$mail->IsHTML (true);
+		if(!$mail->Send())
+		{
+			echo "Mailer Error: " . $mail->ErrorInfo;
+		}
+	}
+
+}
+
+?>
