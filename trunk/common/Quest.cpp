dd2c9f26965a64fa1a435ff50de6c1e53cddcb1a
vdelage <vdelage@3806491c-8dad-11de-9a8c-6d5b7d1e4d13>
2010-02-12 15:59:09 +0000

    added quest part 1

--- trunk/common/Quest.cpp
+++ trunk/common/Quest.cpp
@@ -23,3 +23,45 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "Quest.h"
+
+
+/***********************************************************
+constructor
+***********************************************************/
+Quest::Quest(long QuestId, long titleTextId, long descriptionTextId,
+		std::vector<ConditionBasePtr> conditionsToSucceed,
+		std::vector<long> questsStartingAtStart,
+		std::vector<long> questsStartingAtEnd,
+		std::vector<long> questsTriggeredAtEnd,
+
+		std::vector<std::pair<long, int> > objectsGivenAtEnd,
+		std::vector<std::pair<long, int> > objectsTakenAtEnd)
+
+	: _QuestId(QuestId), _titleTextId(titleTextId), 
+		_descriptionTextId(descriptionTextId), _conditionsToSucceed(),
+		_questsStartingAtStart(questsStartingAtStart),
+		_questsStartingAtEnd(questsStartingAtEnd),
+		_questsTriggeredAtEnd(questsTriggeredAtEnd),
+		_objectsGivenAtEnd(objectsGivenAtEnd),
+		_objectsTakenAtEnd(objectsTakenAtEnd)
+{
+
+}
+
+
+
+
+/***********************************************************
+check if condition are passed to finish quest
+***********************************************************/
+bool Quest::CheckCondition()
+{
+	for(size_t i=0; i<_conditionsToSucceed.size(); ++i)
+	{
+		if(!_conditionsToSucceed[i]->Passed())
+			return false;
+	}
+
+	return true;
+}
+
